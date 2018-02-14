d61f15e3550d7e83115c11e24ee4213aa88f2089
Devon <devonbuckingham2018@u.northwestern.edu>
2018-02-14 16:45:13 -0600

    style improvements

--- search-bar.js
+++ search-bar.js
@@ -1,21 +1,18 @@
 function applySearch(listName) {
-	var searchBox, filter, list, listItems, currentItem, i;
-	searchBox = document.getElementById("search");
-	filter = searchBox.value.toUpperCase();
-	list = document.getElementById(listName);
-	//list = document.getElementById("recipe-ul");
-	listItems = list.getElementsByTagName('li');
-
-	for (i = 0; i < listItems.length; i++)
-	{
-		currentItem = listItems[i]; // this will need to be updated once we have list items implemented to pull the recipe name
-		if (currentItem.innerHTML.toUpperCase().indexOf(filter) > -1)
+	let searchBox, filter, list, currentItem
+	searchBox = document.getElementById("search")
+	filter = searchBox.value.toUpperCase()
+	list = Array.from(document.getElementById(listName).getElementsByTagName('li'))
+	
+	list.forEach(listItem => {
+		titleElement = listItem // this will need to pull something from listItem once recipe list items implemented
+		if (titleElement.innerHTML.toUpperCase().indexOf(filter) > -1)
 		{
-			listItems[i].style.display = "";
+			listItem.style.display = ""
 		}
 		else
 		{
-			listItems[i].style.display = "none";
+			listItem.style.display = "none"
 		}
-	}
+	})
 }
