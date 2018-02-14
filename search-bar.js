e46e4f5e2e155f5bbff8553ab74875cbfc58b230
Devon <devonbuckingham2018@u.northwestern.edu>
2018-02-14 16:10:05 -0600

    Basic filtering implemented. List to filter is currently hardcoded in js function, and assumes that the only content of the list items is the text to filter by

---
+++ search-bar.js
@@ -0,0 +1,21 @@
+function applySearch() {
+	var searchBox, filter, list, listItems, currentItem, i;
+	searchBox = document.getElementById("search");
+	filter = searchBox.value.toUpperCase();
+	//list = document.getElementById(listName);
+	list = document.getElementById("recipe-ul");
+	listItems = list.getElementsByTagName('li');
+
+	for (i = 0; i < listItems.length; i++)
+	{
+		currentItem = listItems[i]; // this will need to be updated once we have list items implemented to pull the recipe name
+		if (currentItem.innerHTML.toUpperCase().indexOf(filter) > -1)
+		{
+			listItems[i].style.display = "";
+		}
+		else
+		{
+			listItems[i].style.display = "none";
+		}
+	}
+}
