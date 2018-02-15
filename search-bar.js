55f3496166ba65a85d55af1f508ffc1db857ea74
Devon <devonbuckingham2018@u.northwestern.edu>
2018-02-15 17:04:47 -0600

    addressed changes mentioned in Jeff's pull request comments

--- search-bar.js
+++ search-bar.js
@@ -1,11 +1,10 @@
 function applySearch(listName) {
-	let searchBox, filter, list, currentItem
-	searchBox = document.getElementById("search")
-	filter = searchBox.value.toUpperCase()
-	list = Array.from(document.getElementById(listName).getElementsByTagName('li'))
+	const searchBox = document.getElementById("search")
+	const filter = searchBox.value.toUpperCase()
+	const list = Array.from(document.getElementById(listName).getElementsByTagName('li'))
 	
 	list.forEach(listItem => {
-		titleElement = listItem // this will need to pull something from listItem once recipe list items implemented
+		const titleElement = listItem // this will need to pull something from listItem once recipe list items implemented
 		if (titleElement.innerHTML.toUpperCase().indexOf(filter) > -1)
 		{
 			listItem.style.display = ""
