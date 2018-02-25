fe1325a6fa61bf5e3700504679ec19385164bd65
Devon <devonbuckingham2018@u.northwestern.edu>
2018-02-25 17:44:37 -0600

    added a cute prof pic for jeff

--- src/js/login-page.js
+++ src/js/login-page.js
@@ -3,7 +3,7 @@ import { loginFormHandler, CreateAllUsers } from './login-utils.js'
 document.getElementById('submit').addEventListener('click', loginFormHandler)
 const usernames = [
     { name: 'Devon', password: 'hi', imageSource: '../../images/devon-pro-pic.jpg' },
-    { name: 'Jeff'},
+    { name: 'Jeff', imageSource: '../../images/jeff.jpg' },
     { name: 'JoshuaShiXXD420Blazze', imageSource: '../../images/josh.jpg'}
 ]
 
