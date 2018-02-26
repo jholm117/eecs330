1bfc7263a26adde0949f108e1fe6395a5d15e384
jholm117 <jholm117@gmail.com>
2018-02-25 21:15:45 -0600

    now you can log in as a few different users (#22)

--- src/js/login-utils.js
+++ src/js/login-utils.js
@@ -1,22 +1,13 @@
-
-export const addLoginHandler = (id, username) =>{
-	const loginHandler = (name) =>{
-        if(!localStorage.hasOwnProperty(username)){
-            createUser(name)
-        }
-		setCurrentUser(name)
-	}
-	document.getElementById(id).addEventListener('click', ()=>loginHandler(username))
-}
-
-const createUser = (username) =>{
+const createUser = ({ name, password = 'password', imageSource = '../../images/blankProfile.png' }) =>{
 	const userObject = JSON.stringify({
-		name: username,
+		name,
+		password,
+		imageSource,
 		favoriteRecipes: [],
 		listRecipes: [],
     })
     
-	localStorage.setItem(username, userObject)
+	localStorage.setItem(name, userObject)
 }
 
 const setCurrentUser = (name) => {
@@ -35,4 +26,53 @@ export const getCurrentUser = () =>{
 
 export const updateUser = (user) => {
     localStorage.setItem(user.name, JSON.stringify(user))
-} 
+}
+
+export const loginFormHandler = () => {
+	const username = document.getElementById('usernameInput').value
+	const password = document.getElementById('passwordInput').value
+	if(userExists(username) && isValidPassword(username,password)){
+		setCurrentUser(username)
+		navigateToRecipeFinder()
+	} else {
+		handleFailedLogin()
+	}
+}
+
+function navigateToRecipeFinder(){
+	window.location = 'recipe-finder.html'
+}
+
+function userExists(name){
+	return localStorage.hasOwnProperty(name)
+}
+
+function isValidPassword(name, password){
+	const jsonUser = localStorage.getItem(name)
+	const user = JSON.parse(jsonUser)
+	return user.password === password
+}
+
+export function CreateAllUsers(usernames){
+    usernames.forEach(user=>{
+        if(!userExists(user.name)){
+            createUser(user)
+        }
+    })
+}
+
+function handleFailedLogin(){
+	alert("Invalid Credentials")
+	document.getElementById('usernameInput').value = ""
+	document.getElementById('passwordInput').value = ""
+}
+
+export function redirectIfLoggedOut(){
+	if(!sessionStorage.hasOwnProperty('currentUser') || sessionStorage.getItem('currentUser') === null){
+		window.location = 'index.html'
+	}
+}
+
+export function logOut(){
+	sessionStorage.setItem('currentUser', null)
+}
