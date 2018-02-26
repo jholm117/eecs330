ccdcb255d03f822bd8397d4e1c153ef004670043
jholm117 <jholm117@gmail.com>
2018-02-25 22:16:30 -0600

    changed image pathnames

--- src/js/login-page.js
+++ src/js/login-page.js
@@ -2,9 +2,9 @@ import { loginFormHandler, CreateAllUsers } from './login-utils.js'
 
 document.getElementById('submit').addEventListener('click', loginFormHandler)
 const usernames = [
-    { name: 'Devon', password: 'hi', imageSource: '../../images/devon-pro-pic.jpg' },
-    { name: 'Jeff', imageSource: '../../images/jeff.jpg' },
-    { name: 'JoshuaShiXXD420Blazze', imageSource: '../../images/josh.jpg'}
+    { name: 'Devon', password: 'hi', imageSource: 'images/devon-pro-pic.jpg' },
+    { name: 'Jeff', imageSource: 'images/jeff.jpg' },
+    { name: 'JoshuaShiXXD420Blazze', imageSource: 'images/josh.jpg'}
 ]
 
 CreateAllUsers(usernames)
