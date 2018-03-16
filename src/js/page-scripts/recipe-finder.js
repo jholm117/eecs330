af3da8484237fca08ffe7499bf487d64a7a56c38
jholm117 <jholm117@gmail.com>
2018-03-15 23:27:26 -0500

    favorites button changes color

--- src/js/page-scripts/recipe-finder.js
+++ src/js/page-scripts/recipe-finder.js
@@ -1,36 +1,10 @@
 import recipes from '../recipes.js'
-import { addToShoppingList, addToSaved, addSearchHandler, populateList, removeFromSaved, addFavoriteTag } from '../utils/recipe-util.js'
+import { addToShoppingList, addToSaved, addSearchHandler, populateRecipeFinderList, removeFromSaved, addFavoriteTag, finderButtons } from '../utils/recipe-util.js'
 import { redirectIfLoggedOut, getCurrentUser } from '../utils/login-utils.js';
 import { addNavToPage } from '../utils/nav-utils.js';
 
 const recipeFinderListId = "recipe-ul"
 const favoritesId = "favorites-ul"
-const finderButtons = {
-	top: {
-		icon: "favorite",
-		text: "Favorite",
-		onClick: addToSaved,
-	},
-	bottom: {
-		icon: "playlist_add",
-		text: "Add to List",
-		onClick: addToShoppingList,
-	},
-}
-
-const favoritesButtons =  {
-	top: {
-		icon: "clear",
-		text: "Unfavorite",
-		onClick: (id) => removeFromSaved(id,favoritesId),
-	},
-	bottom: {
-		icon: "playlist_add",
-		text: "Add to List",
-		onClick: addToShoppingList,
-	},
-}
-
 
 function filterSelection(selector) {
 	const recipeItems = Array.from(document.getElementById(recipeFinderListId).getElementsByTagName("li"))
@@ -58,7 +32,7 @@ function markFavorites() {
 redirectIfLoggedOut()
 
 addNavToPage()
-populateList(Object.keys(recipes), finderButtons, recipeFinderListId)
+populateRecipeFinderList(Object.keys(recipes), recipeFinderListId)
 markFavorites()
 addHandlersToFilters()
 filterSelection("All")
--- src/js/utils/recipe-util.js
+++ src/js/utils/recipe-util.js
@@ -25,7 +25,7 @@ function buildRecipe(recipeInfo, buttons) {
 							</div>
 						</div>
 						<div class="btn-group-vertical recipe-buttons">
-							<button class="btn btn-outline-secondary top-button border-0"><i class="material-icons">${buttons.top.icon}</i><span>${buttons.top.text}</span></button>
+							<button class="btn btn-outline-${buttons.top.color} top-button border-0"><i class="material-icons">${buttons.top.icon}</i><span>${buttons.top.text}</span></button>
 							<button class="btn btn-outline-secondary border-0"><i class="material-icons">${buttons.bottom.icon}</i><span>${buttons.bottom.text}</span></button>
 						</div>
 			  </div>
@@ -35,6 +35,19 @@ function buildRecipe(recipeInfo, buttons) {
 	recipeButtons[0].addEventListener("click", buttons.top.onClick.bind(null,recipeInfo.id))
 	recipeButtons[1].addEventListener("click", buttons.bottom.onClick.bind(null,recipeInfo.id))	
 	return template
+}
+
+export function swapTopButton(id,button){
+	const icon = `<i class="material-icons">${button.icon}</i><span>${button.text}</span>`
+	const redButton = document.createElement("button")
+	redButton.className = `btn btn-outline-${button.color} top-button border-0`
+	redButton.innerHTML = icon
+	redButton.addEventListener('click', button.onClick.bind(null, id))
+
+	const card = document.getElementById(id)
+	const favoriteButton = card.getElementsByClassName('top-button')[0]
+	card.getElementsByClassName('recipe-buttons')[0].insertBefore(redButton,favoriteButton)
+	favoriteButton.remove()
 }
 
 function buildIngredientCard(ingredientInfo){
@@ -81,45 +94,6 @@ function buildIngredientCard(ingredientInfo){
 	return template
 }
 
-
-// function buildIngredientCard(ingredientInfo){
-// 	const template = document.createElement("li")
-// 	template.innerHTML = `<div class="card-shadow my-card ingredient-card">
-// 								<div class="card-body">
-// 									<div class="recipe-info">
-// 										<h4 class="card-title">${ingredientInfo.item.name}</h2>
-// 									</div>
-// 									<div class="ingredient-toolbar">										
-// 										<span class="input-group plus-minus">
-// 											<div class="input-group-prepend">
-// 												<button class="btn btn-outline-secondary minus-button" type="button">-</button>
-// 											</div>
-// 											<input type="text" class="form-control" value="${ingredientInfo.amount}">
-// 											<div class="input-group-append">
-// 												<button class="btn btn-outline-secondary plus-button" type="button">+</button>
-// 											</div>
-// 										</span>
-// 										<span class="units">${ingredientInfo.item.units}</span>
-// 										<button class="btn btn-danger ex border-0"><i class="material-icons">clear</i></button>										
-// 									</div>
-// 								</div>
-// 							</div>`
-// 	const input = template.getElementsByTagName("input")[0]
-// 	const plus = template.getElementsByClassName("plus-button")[0]
-// 	const minus = template.getElementsByClassName("minus-button")[0]
-// 	const changeValue = (inputField, amount) => {
-// 		input.value = parseInt(input.value) + amount
-// 		if(input.value <= 0) template.style.display = "none"
-// 	}
-// 	input.addEventListener('change', ()=>{
-// 		if(input.value <= 0) template.style.display = "none"
-// 	})
-// 	plus.addEventListener('click', ()=>changeValue(input,1))
-// 	minus.addEventListener('click', ()=>changeValue(input,-1))
-	
-// 	return template
-// }
-
 export function updateList(listId, buttons){
 	const IDs = Array.from(document.getElementById(listId).children).map(li => li.id)
 	const favorites = getCurrentUser().favoriteRecipes
@@ -137,6 +111,20 @@ export const populateShoppingList = (idList, buttons, listId) => {
 	const ingredients = idList.map(id => recipes[id].ingreds.map(buildIngredientCard))
 		.reduce((prev, curr) => prev.concat(curr), [])	
 	ingredients.forEach(node => document.getElementById(listId).appendChild(node))
+}
+
+export const populateRecipeFinderList = (idList, listId) => {
+	idList.forEach(id =>{
+		let buttons = finderButtons
+		if(getCurrentUser().favoriteRecipes.indexOf(id) > -1){
+			buttons = {
+				top: unfavoriteButton,
+				bottom: finderButtons.bottom,
+			}
+		} 
+		const recipeNode = buildRecipe(recipes[id], buttons)
+		document.getElementById(listId).appendChild(recipeNode)
+	})
 }
 
 export const populateList = (idList, buttons, listId) => {
@@ -166,12 +154,11 @@ export function removeFromShoppingList(recipeId,listId){
 export function addToSaved(recipeId) {
 	addFavoriteTag(recipeId)
 	addToList(favoriteRecipesKey,recipeId)	
-	alert("Recipe added to your favorites")
 }
 
 export function removeFromSaved(recipeId, favoritesId) {
 	removeFavoriteTag(recipeId)
-	removeFromList(favoriteRecipesKey,recipeId, favoritesId)
+	removeFromList(favoriteRecipesKey,recipeId, favoritesId, true)
 }
 
 
@@ -193,8 +180,8 @@ function addToList(listKey,recipeId) {
 	}
 }
 
-function removeFromList(listKey,recipeId, listId) {
-	document.querySelector(`#${listId} #${recipeId}`).outerHTML = ""
+function removeFromList(listKey,recipeId, listId, isFavorite = false) {
+	if(!isFavorite)document.querySelector(`#${listId} #${recipeId}`).outerHTML = ""
 	const user = getCurrentUser()
 	user[listKey] = user[listKey].filter(id => id !== recipeId)
 	updateUser(user)
@@ -228,4 +215,34 @@ export function makeInvisible(id){
 
 export function makeVisible(id){
 	document.getElementById(id).style.display = "block"
+}
+
+export const finderButtons = {
+	top: {
+		icon: "favorite",
+		text: "Favorite",
+		color: "secondary",
+		onClick: (id) => {
+			addToSaved(id)
+			swapTopButton(id, unfavoriteButton)
+		},
+	},
+	bottom: {
+		icon: "playlist_add",
+		text: "Add to List",
+		color: "secondary",
+		onClick: (id) => { 
+			addToShoppingList(id)
+		},
+	},
+}
+
+const unfavoriteButton = {
+	icon: "favorite",
+	text: "Unfavorite",
+	color: "danger",
+	onClick: (id) =>{
+		swapTopButton(id, finderButtons.top)
+		removeFromSaved(id)
+	}
 }
