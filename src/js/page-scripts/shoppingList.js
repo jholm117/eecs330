1bd94b553657b8f14d1f852e19285791ae9d6144
jholm117 <jholm117@gmail.com>
2018-03-15 20:55:10 -0500

    stuff and things (#38)
    * stuff and things
    
    * hope it works!

--- src/js/page-scripts/shoppingList.js
+++ src/js/page-scripts/shoppingList.js
@@ -1,9 +1,31 @@
-import { removeFromShoppingList, addToSaved, addSearchHandler, populateList } from '../utils/recipe-util.js'
+import { removeFromShoppingList, addToSaved, addSearchHandler, populateShoppingList } from '../utils/recipe-util.js'
 import { getCurrentUser, redirectIfLoggedOut } from '../utils/login-utils.js'
 import { addNavToPage } from '../utils/nav-utils.js';
 
 const user = getCurrentUser()
 const shoppingListId = 'shopping-list-recipes'
+const recipeFilterId = 'recipe-filter'
+const ingredientFilterId = 'ingredient-filter'
+
+function recipeFilterHandler(){
+	filterSelection("recipe-card")
+}
+
+function ingredientFilterHandler(){
+	filterSelection("ingredient-card")
+}
+
+function filterSelection(selector) {
+	const recipeItems = Array.from(document.getElementById(shoppingListId).getElementsByTagName("li"))
+    recipeItems.forEach(item => {    
+        item.style.display = item.firstChild.className.indexOf(selector) > -1 ? "block" : "none" 
+    })
+}
+
+function addFilterHandlers(){
+	document.getElementById(recipeFilterId).addEventListener('click', recipeFilterHandler)
+	document.getElementById(ingredientFilterId).addEventListener('click', ingredientFilterHandler)
+}
 
 const buttons = {
 	top: {
@@ -20,5 +42,6 @@ const buttons = {
 redirectIfLoggedOut()
 
 addNavToPage()
-populateList(user.listRecipes, buttons, shoppingListId)
+populateShoppingList(user.listRecipes, buttons, shoppingListId)
 addSearchHandler(shoppingListId)
+addFilterHandlers()
--- src/js/recipes.js
+++ src/js/recipes.js
@@ -1,3 +1,25 @@
+const ingredients = {
+	"chickenThighs": {
+		name: "Chicken Thighs (Bone-in)",
+		units: "pounds",
+	},
+	"ginger": {
+		name: "Ginger",
+		units: "tablespoons",
+	},
+	"soy": {
+		name: "Soy Sauce",
+		units: "tablespoons",
+	},
+	"broccoli": {
+		name: "Broccoli",
+		units: "crowns",
+	},
+	"garlic":{
+		name: "Garlic",
+		units: "cloves",
+	},	
+} 
 const recipes = {
 	"gingerSoyChicken" : {
 		name: "Ginger Soy Chicken",
@@ -5,35 +27,59 @@ const recipes = {
 		imgSrc: "images/chicken.jpg",
 		cookTime: "30 minutes",
 		calories: "600 calories",
-		tags: ["","Asian","Cheap"]},
+		tags: ["","Asian","Cheap"],
+		ingreds: [
+			{ item: ingredients.chickenThighs, amount: 4 },
+			{ item: ingredients.ginger, amount: 4 },
+			{ item: ingredients.soy, amount: 4 },
+		],
+	},
 	"garlicRoastedBroccoli" : {
 		name: "Garlic Roasted Broccoli",
 		id: "garlicRoastedBroccoli",
 		imgSrc: "images/garlicRoastedBroccoli.jpg",
 		cookTime: "20 minutes",
 		calories: "150 calories",
-		tags: ["","Vegetarian"]},
+		tags: ["","Vegetarian"],
+		ingreds: [
+			{ item: ingredients.garlic, amount: 2 },
+			{ item: ingredients.broccoli, amount: 4 },
+		],
+	},
 	"squashTaco" : {
 		name: "Roasted Butternut Squash Tacos",
 		id: "squashTaco",
 		imgSrc: "images/squashTaco.jpg",
 		cookTime: "50 minutes",
 		calories: "400 calories",
-		tags: ["","Mexican","Vegetarian","Cheap"]},
+		tags: ["","Mexican","Vegetarian","Cheap"],
+		ingreds: [
+			// { item: ingredients.chickenThighs, amount: 4 },
+		],
+	},
 	"shrimpSoup" : {
 		name: "Shrimp Tomato Soup",
 		id: "shrimpSoup",
 		imgSrc: "images/shrimpSoup.jpg",
 		cookTime: "50 minutes",
 		calories: "520 calories",
-		tags: ["","Fusion"]},
+		tags: ["","Fusion"],
+		ingreds: [
+			{ item: ingredients.chickenThighs, amount: 4 },
+		],
+	},
 	"porkChop" : {
 		name: "Cinnamon Chipotle Pork Chops",
 		id: "porkChop",
 		imgSrc: "images/porkChop.jpg",
 		cookTime: "25 minutes",
 		calories: "800 calories",
-		tags: ["","Mexican","Fusion"]}
+		tags: ["","Mexican","Fusion"],
+		ingreds: [
+			{ item: ingredients.chickenThighs, amount: 4 },
+		],
+	},
 }
+
 
 export default recipes
